52918a9d6362ca067531a3a4df704e694a014350
Fabianacafé <fabicomercial2@gmail.com>
2022-07-20 11:20:30 -0300

    meu primeiro commit

---
+++ PrimeiroProgramaJava/src/aulas/HelloWord.java
@@ -0,0 +1,33 @@
+package aulas;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int minhaVariavel = 10;
+		String minhaString = "Fabi";
+		double minhaDouble = 38.25;
+		char minhaChar = 'F';
+		boolean verdadeiro = true;
+		boolean falso = false;
+
+		System.out.println("Hello Word");
+		System.out.println(minhaVariavel);
+		System.out.println(minhaString);
+		System.out.println(minhaDouble);
+		System.out.println(minhaChar);
+		System.out.println(verdadeiro);
+		System.out.println(falso);
+		System.out.println(minhaVariavel + " " + minhaString + " " + minhaDouble);
+		System.out.println(minhaString + "\n" + verdadeiro);
+		
+		
+		System.out.print("Aqui");
+		System.out.print(" vai uma");
+		System.out.print(" mensagem!");
+		
+	}
+
+}
